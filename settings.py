3cced5e77d163500a8f0563a19cdf4c61f43d724
Terence Gatt <gatt@hydra.to>
2017-12-20 04:55:38 +0000

    Added settings.py to ignore list.

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^*yx+(y$mm++2dbh-uxx+#edxp__e=3#9(1&h^+yh7n9@_&*=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
